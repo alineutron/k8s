5e65081b9c7daa502e280381067ae4e4f6016a2b
asad.mirza <asad.mirza@calabrio.com>
2021-11-16 13:26:17 +0100

    Added a playground

---
+++ dockerplayground/dockerplayground/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using dockerplayground.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dockerplayground.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if (!System.IO.File.Exists("c:/storage/playground.txt"))
+            {
+                System.IO.Directory.CreateDirectory("c:/storage/");
+                System.IO.File.Create("c:/storage/playground.txt");
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
